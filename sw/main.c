fc0c18e2fd8f03cc069cdb748bb624059f6b4e69
Matías Silva Bustos <matiasasb@gmail.com>
2017-05-01 18:06:47 -0300

    Add just UARTCharPut on main_loop().

--- sw/main.c
+++ sw/main.c
@@ -1,13 +1,14 @@
 
 #include <soc_C6748.h>
 #include <interrupt.h>
-
+#include <uart.h>
 extern void inicializarUART();
 
 static inline void main_loop()
 {
-
+    UARTCharPut(SOC_UART_2_REGS, 'A');
 }
+
 void main()
 {
     // Initialize the DSP INTC
@@ -15,8 +16,10 @@ void main()
 
     inicializarUART();
 
+#if 1
     // Enable DSP interrupts globally
     IntGlobalEnable();
+#endif
 
     while(1)
         main_loop();
